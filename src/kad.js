da8b17b9ecbea51e1b27b5ff0e05f531a9e73d2d
maowc <mwc@foxmail.com>
2014-06-01 17:05:07 +0800

    * commit

---
+++ src/kad.js
@@ -0,0 +1,14 @@
+﻿/**
+* Copyright 2014 MWC
+* https://github.com/zm-kad/kad
+*
+* kad.js v0.1 - 2014/6/1
+*
+* Released under the MIT license.
+*/
+var kad;
+(function (kad) {
+    kad.mix = function () {
+    };
+})(kad || (kad = {}));
+//# sourceMappingURL=kad.js.map
---
+++ src/kad.ts
@@ -0,0 +1,15 @@
+﻿/**
+ * Copyright 2014 MWC
+ * https://github.com/zm-kad/kad
+ * 
+ * kad.js v0.1 - 2014/6/1
+ * 
+ * Released under the MIT license.
+ */
+
+module kad
+{
+    export var mix = () =>
+    {
+    };
+}
