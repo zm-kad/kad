8e2911af6881cbe58951838ee89548b73a2bb8ce
maowc <mwc@foxmail.com>
2014-06-08 23:22:20 +0800

    kad

--- release/kad.js
+++ release/kad.js
@@ -6,19 +6,9 @@
 
     kad.merge = function (dest, src, deep) {
         for (var prop in src) {
-            if (deep) {
-                dest[prop] = kad.merge(dest[prop], src[prop], deep);
-            }
-
             dest[prop] = src[prop];
         }
 
         return dest;
-    };
-
-    kad.extend = function () {
-        if (arguments.length === 1) {
-            return kad.merge(kad, arguments[0]);
-        }
     };
 })(kad || (kad = {}));
--- src/kad.ts
+++ src/kad.ts
@@ -7,22 +7,56 @@
  * Released under the MIT license.
  */
 
-( ( g, doc: Document, undefined?) =>
+module kad
 {
-    if ( g.kad && g.kad.zmwc )
-        return;
+    "use strict";
 
-    var kd: any = g.kad = { zmwc: 1 };
-    
-    // 扩展指定对象
-    kd.mix = ( dest: Object, src: Object, deep?: boolean ) =>
-    {
-        for ( var item in src )
+    var g = window,
+        doc = document;
+
+    export var merge =
+        /**
+         * 将 src 对象合并到 dest 中，合并后返回 dest 对象。
+         * 
+         * @param dest  目标对象，将作为返回结果。
+         * @param src   合并源
+         * @param deep  [可选] 是否进行深拷贝
+         */
+        ( dest: Object, src: Object, deep?: boolean ) =>
         {
-            dest[item] = src[item];
+            for ( var prop in src )
+            {
+                if ( deep )
+                {
+                    dest[prop] = merge( dest[prop], src[prop], deep );
+                }
+
+                dest[prop] = src[prop];
+            }
+
+            return dest;
         }
 
-        return dest;
-    };
+    export var extend: {
+        /**
+         * 扩展指定对象，将从右向左扩展，支持同时给出多个对象
+         * 
+         * @param args 指定要扩展的对象，从右向左合并，若仅提供一个对象，则扩展 kad 自身
+         */
+        ( ...args: Object[] ): Object;
 
-})( window, document );
+        /**
+         * 扩展指定对象，将从右向左扩展，支持同时给出多个对象
+         * 
+         * @param deep 是否进行深拷贝
+         * @param args 指定要扩展的对象，从右向左合并，若仅提供一个对象，则扩展 kad 自身
+         */
+        ( deep: boolean, ...args: Object[] ): Object;
+    } = () =>
+        {
+            if ( arguments.length === 1 )
+            {
+                return merge( kad, arguments[0] );
+            }
+        }
+}
